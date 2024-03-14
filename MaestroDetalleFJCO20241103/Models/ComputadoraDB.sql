ebdb0894658b0e7554150a82bd56b79adc6bf920
FJ <106713549+XxFJxX@users.noreply.github.com>
2024-03-13 22:07:47 -0600

    Add files via upload

---
+++ MaestroDetalleFJCO20241103/Models/ComputadoraDB.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE Computadora
+GO
+
+USE Computadora
+
+-- Crear la tabla 'Computadora'
+CREATE TABLE Computadora (
+    id INT PRIMARY KEY IDENTITY,
+    nombre VARCHAR(100),
+    marca VARCHAR(50),
+    precio DECIMAL(10, 2)
+);
+
+-- Crear la tabla 'Componente'
+CREATE TABLE Componente (
+    id INT PRIMARY KEY IDENTITY,
+    computadora_id INT,
+    nombre VARCHAR(100),
+    tipo VARCHAR(50),
+    FOREIGN KEY (computadora_id) REFERENCES Computadora(id) ON DELETE CASCADE
+);
